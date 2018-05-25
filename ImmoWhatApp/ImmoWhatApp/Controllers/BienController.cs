fc3abf07a422bfe5e791b5cdc6f9b73650115dfe
JoinauThomas <thomasjoinau@gmail.com>
2018-05-25 17:05:58 +0200

    reprise pour TFE

--- ImmoWhatApp/ImmoWhatApp/Controllers/BienController.cs
+++ ImmoWhatApp/ImmoWhatApp/Controllers/BienController.cs
@@ -26,6 +26,26 @@ namespace ImmoWhatApp.Controllers
                 new Models.TypeBienModels{idTypeBien = 4, libelle = Resource.appartement}
             };
 
+            List<Models.nbPieces> nbPieces = new List<Models.nbPieces>
+            {
+                new Models.nbPieces{qtite = 0, affiche = "0"},
+                new Models.nbPieces{qtite = 1, affiche = "1"},
+                new Models.nbPieces{qtite = 2, affiche = "2"},
+                new Models.nbPieces{qtite = 3, affiche = "3"},
+                new Models.nbPieces{qtite = 4, affiche = "4"},
+                new Models.nbPieces{qtite = 5, affiche = "5"},
+                new Models.nbPieces{qtite = 6, affiche = "6"},
+                new Models.nbPieces{qtite = 7, affiche = "7"},
+                new Models.nbPieces{qtite = 8, affiche = "8"},
+                new Models.nbPieces{qtite = 9, affiche = "9"},
+                new Models.nbPieces{qtite = 10, affiche = "10+"},
+            };
+
+            List<string>noteNRJ = new List<string> { "--","A", "B", "C", "D", "E", "F", "G"};
+
+
+            ViewBag.nrj = new SelectList(noteNRJ);
+            ViewBag.nbPieces = new SelectList(nbPieces, "qtite", "affiche");
             ViewBag.ListeTypesBiens = new SelectList(typesBiensList, "idTypeBien", "libelle");
             return View();
         }
---
+++ ImmoWhatApp/ImmoWhatApp/Models/nbPieces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImmoWhatApp.Models
+{
+    public class nbPieces
+    {
+        public int qtite { get; set; }
+        public string affiche { get; set; }
+    }
+}
